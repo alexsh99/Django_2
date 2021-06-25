576e5e1975417a4bb9a88e46c85a5db46484f828
Александр <ash@isnet.ru>
2021-06-25 16:33:09 +0500

    Lesson 2

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -1,11 +1,10 @@
-import logging
 from django.conf import settings
 from django.contrib import auth
 from django.core.mail import send_mail
 from django.shortcuts import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-from .forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm
+from .forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm, ShopUserProfileEditForm
 from .models import ShopUser
 
 
@@ -36,12 +35,14 @@ def edit(request):
         return HttpResponseRedirect(reverse('authapp:login'))
     if request.method == 'POST':
         edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
-        if edit_form.is_valid():
+        profile_form = ShopUserProfileEditForm(request.POST, instance=request.user.shopuserprofile)
+        if edit_form.is_valid() and profile_form.is_valid():
             edit_form.save()
             return HttpResponseRedirect(reverse('authapp:edit'))
     else:
         edit_form = ShopUserEditForm(instance=request.user)
-    context = {'title': title, 'edit_form': edit_form}
+        profile_form = ShopUserProfileEditForm(instance=request.user.shopuserprofile)
+    context = {'title': title, 'edit_form': edit_form, 'profile_form': profile_form}
     return render(request, 'edit.html', context=context)
 
 
